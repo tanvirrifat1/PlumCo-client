2eb3952d322689f6bb8a999b36419e959f8454b1
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-10-15 12:22:37 +0600

    added contact

--- src/app/Contacts/page.tsx
+++ src/app/Contacts/page.tsx
@@ -2,6 +2,7 @@
 
 import ContactHeader from "@/components/ui/Contact/ContactHeader";
 import FormContact from "@/components/ui/Contact/FormContact";
+import MapContainer from "@/components/ui/Contact/MapContainer";
 import Footer from "@/components/ui/Footer";
 import Navbar from "@/components/ui/Navbar";
 import React from "react";
@@ -12,6 +13,7 @@ const page = () => {
       {/* <Navbar /> */}
       <ContactHeader />
       <FormContact />
+      <MapContainer />
       {/* <Footer /> */}
     </div>
   );
