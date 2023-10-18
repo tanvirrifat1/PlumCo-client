1f70dad6b1230bd1e05d5cafbb9baf5449f0fb2a
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-10-18 20:57:51 +0600

    added category

--- src/app/(withlayout)/service/[id]/page.tsx
+++ src/app/(withlayout)/service/[id]/page.tsx
@@ -4,10 +4,11 @@ import Loading from "@/app/loading";
 import { useGetSingleDataQuery } from "@/redux/api/serviceApi";
 import { isLoggedin } from "@/service/auth.service";
 import Image from "next/image";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
 import { GrLinkNext } from "react-icons/gr";
+
+import img from "../../../../assets/dashboard.png";
 
 const page = ({ params }: { params: any }) => {
   const { id } = params;
@@ -31,21 +32,18 @@ const page = ({ params }: { params: any }) => {
   }
 
   return (
-    <section className="p-6 bg-slate-600">
+    <section className="p-6 bg-slate-100">
       <div className="container grid gap-6 mx-auto text-center lg:grid-cols-2 xl:grid-cols-5">
-        <div className="w-full px-6 py-16 rounded-md sm:px-12 md:px-16 xl:col-span-2 dark:bg-gray-900">
-          <h1 className="text-5xl font-extrabold dark:text-gray-50">
-            {data?.title}
-          </h1>
+        <div className="w-full px-6 py-16 rounded-md sm:px-12 md:px-16 xl:col-span-2 ">
+          <h1 className="text-5xl font-extrabold ">{data?.title}</h1>
           <p className="my-8">
-            <span className="font-medium dark:text-gray-50">
-              {data?.description}
-            </span>
+            <span className="font-medium ">{data?.description}</span>
+          </p>
+          <p className="my-2">
+            <span className="font-medium ">Price: {data?.price}$</span>
           </p>
           <p className="my-8">
-            <span className="font-medium dark:text-gray-50">
-              If choice this service please
-            </span>
+            <span className="font-medium ">If choice this service please</span>
           </p>
           <div>
             <button
@@ -55,6 +53,18 @@ const page = ({ params }: { params: any }) => {
               Book Now
               <GrLinkNext className="text-2xl hover:text-white" />
             </button>
+          </div>
+          <div className="flex justify-center gap-2">
+            <div className="card w-72 bg-base-100 rounded-md shadow-xl mt-4">
+              <figure>
+                <Image src={data?.image} alt="Shoes" width={300} height={300} />
+              </figure>
+            </div>
+            <div className="card w-72 bg-base-100 rounded-md shadow-xl mt-4">
+              <figure>
+                <Image src={img} alt="Shoes" width={300} height={300} />
+              </figure>
+            </div>
           </div>
         </div>
         <Image
