bbd8a6c33f339cf60a2c84cab4353d8f67a20e16
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-12-12 22:24:20 +0600

    added

--- src/app/(withlayout)/blog/[id]/page.tsx
+++ src/app/(withlayout)/blog/[id]/page.tsx
@@ -22,7 +22,7 @@ const DetailsBlog = ({ params }: { params: any }) => {
   };
 
   return (
-    <div className="bg-base-200">
+    <div className="">
       <Link href={"/home"}>
         <div className="ml-36 mt-10">
           <BiArrowBack className="text-4xl" />
@@ -31,42 +31,41 @@ const DetailsBlog = ({ params }: { params: any }) => {
       <div className="hero min-h-[500px] ">
         <div className="hero-content flex-col lg:flex-row">
           <Image src={data?.image} width={800} height={1000} alt="" />
-          <div>
-            <h1 className="text-5xl font-bold">{data?.title}</h1>
-            <p className="py-6">{data?.content}</p>
-            <div className="divider"></div>
-            <div className="flex justify-between">
-              <div>
-                <div className="flex text-4xl gap-4">
-                  <p className="flex">
-                    <AiOutlineLike onClick={handleIncrement} />
-                    <p className="text-xs text-accent font-semibold text-[20px]">
-                      {quantity}
-                    </p>
+        </div>
+      </div>
+      <div className="mx-8">
+        <div>
+          <h1 className="text-5xl font-bold">{data?.title}</h1>
+          <p className="py-6">{data?.content}</p>
+          <div className="divider"></div>
+          <div className="flex justify-between">
+            <div>
+              <div className="flex text-4xl gap-4">
+                <p className="flex">
+                  <AiOutlineLike onClick={handleIncrement} />
+                  <p className="text-xs text-accent font-semibold text-[20px]">
+                    {quantity}
                   </p>
+                </p>
 
-                  <Link href={"/Contacts"}>
-                    <FaRegComment />
-                  </Link>
-                </div>
+                <Link href={"/Contacts"}>
+                  <FaRegComment />
+                </Link>
               </div>
-              <div>
-                <div className="card-actions justify-start">
-                  <div className="avatar">
-                    <div className="w-14 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                      <Image
-                        src={data?.author?.profileImage}
-                        width={20}
-                        height={20}
-                        alt=""
-                      />
-                    </div>
-                  </div>
-                  <div className=" mt-4 lg:ml-3">
-                    {data?.author?.fullName}
-                    <p className="text-xl mt-2">Author</p>
+            </div>
+            <div>
+              <div className="card-actions justify-start">
+                <div className="avatar">
+                  <div className="w-14 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                    <Image
+                      src={data?.author?.profileImage}
+                      width={20}
+                      height={20}
+                      alt=""
+                    />
                   </div>
                 </div>
+                <div className=" mt-4 lg:ml-3">{data?.author?.fullName}</div>
               </div>
             </div>
           </div>
