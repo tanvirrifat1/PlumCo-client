97b1b644599aafde6d392788058c53951e7c91e7
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2024-01-30 21:12:04 +0600

    added loading

--- src/components/ui/CategoryPage.tsx
+++ src/components/ui/CategoryPage.tsx
@@ -14,8 +14,20 @@ const CategoryPage = () => {
   const handleService = (id: string) => {
     router.push(`/category/service/${id}`);
   };
+
   if (isLoading) {
-    return <Loading />;
+    return (
+      <div className=" flex justify-center items-center my-12">
+        <div className="flex flex-col m-8 rounded shadow-md w-60 sm:w-80 animate-pulse h-96">
+          <div className="h-48 rounded-t dark:bg-gray-700"></div>
+          <div className="flex-1 px-4 py-8 space-y-4 sm:p-8 dark:bg-gray-900">
+            <div className="w-full h-6 rounded dark:bg-gray-700"></div>
+            <div className="w-full h-6 rounded dark:bg-gray-700"></div>
+            <div className="w-3/4 h-6 rounded dark:bg-gray-700"></div>
+          </div>
+        </div>
+      </div>
+    );
   }
   return (
     <div className="container lg:w-[1000px] pt-20 pb-5">
--- src/components/ui/Services.tsx
+++ src/components/ui/Services.tsx
@@ -108,6 +108,17 @@ const Services = () => {
     setSearchTerm("");
   };
 
+  const handleLoading = () => {
+    <div className="flex flex-col m-8 rounded shadow-md w-60 sm:w-80 animate-pulse h-96">
+      <div className="h-48 rounded-t dark:bg-gray-700"></div>
+      <div className="flex-1 px-4 py-8 space-y-4 sm:p-8 dark:bg-gray-900">
+        <div className="w-full h-6 rounded dark:bg-gray-700"></div>
+        <div className="w-full h-6 rounded dark:bg-gray-700"></div>
+        <div className="w-3/4 h-6 rounded dark:bg-gray-700"></div>
+      </div>
+    </div>;
+  };
+
   return (
     <section className="py-10 md:py-20 container">
       <div className="flex flex-wrap">
@@ -148,7 +159,11 @@ const Services = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-5">
         {isLoading ? (
           <>
-            <Loading />
+            {/* <Loading /> */}
+            {handleLoading()}
+            {handleLoading()}
+            {handleLoading()}
+            {handleLoading()}
           </>
         ) : (
           data?.map((service: any) => (
